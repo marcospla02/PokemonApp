7bac72c43b3b6df2acf79516cd323bb8a052dd6f
marcos pla <marcospla413@gmail.com>
2023-03-03 12:34:52 -0300

    extends the input range in detail

--- client/src/pages/Detail/SliderDetail.tsx
+++ client/src/pages/Detail/SliderDetail.tsx
@@ -19,37 +19,68 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme, color }) => ({
 }));
 
 export function ProgressBarsAttack(props: any) {
+  const MIN = 0;
+  const MAX = 200;
   const value = props.attack;
+  const normalise = ((value - MIN) * 100) / (MAX - MIN);
   return (
-    <BorderLinearProgress variant="determinate" value={value} color="error" />
+    <BorderLinearProgress
+      variant="determinate"
+      value={normalise}
+      color="error"
+    />
   );
 }
 
 export function ProgressBarsDefense(props: any) {
   const value = props.defense;
+  const MIN = 0;
+  const MAX = 200;
+  const normalise = ((value - MIN) * 100) / (MAX - MIN);
   return (
-    <BorderLinearProgress variant="determinate" value={value} color="primary" />
+    <BorderLinearProgress
+      variant="determinate"
+      value={normalise}
+      color="primary"
+    />
   );
 }
 export function ProgressBarsSpeed(props: any) {
+  const MIN = 0;
+  const MAX = 200;
   const value = props.speed;
+  const normalise = ((value - MIN) * 100) / (MAX - MIN);
   return (
-    <BorderLinearProgress variant="determinate" value={value} color="warning" />
+    <BorderLinearProgress
+      variant="determinate"
+      value={normalise}
+      color="warning"
+    />
   );
 }
 export function ProgressBarsHeigth(props: any) {
   const value = props.heigth;
+  const MIN = 0;
+  const MAX = 200;
+  const normalise = ((value - MIN) * 100) / (MAX - MIN);
   return (
     <BorderLinearProgress
       variant="determinate"
-      value={value}
+      value={normalise}
       color="secondary"
     />
   );
 }
 export function ProgressBarsLife(props: any) {
   const value = props.life;
+  const MIN = 0;
+  const MAX = 200;
+  const normalise = ((value - MIN) * 100) / (MAX - MIN);
   return (
-    <BorderLinearProgress variant="determinate" value={value} color="success" />
+    <BorderLinearProgress
+      variant="determinate"
+      value={normalise}
+      color="success"
+    />
   );
 }
