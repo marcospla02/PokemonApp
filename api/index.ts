65362c06e287fb99e56822cd1b4f59d839ab563e
marcos pla <marcospla413@gmail.com>
2023-03-07 17:25:41 -0300

    alter false

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { sequelize } from "./src/db";
 
 const PORT = 3001;
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ alter: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
