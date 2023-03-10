42ad781c5c58b7c253cf072de8df7f8577cf2d50
marcos pla <marcospla413@gmail.com>
2023-03-10 16:56:59 -0300

    connection sequelize change

--- api/build/index.js
+++ api/build/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./src/app"));
 const db_1 = require("./src/db");
 const PORT = 3001;
-db_1.sequelize.sync({ alter: false }).then(() => {
+db_1.sequelize.sync({ force: true }).then(() => {
     app_1.default.listen(PORT, () => {
         console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
     });
--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { sequelize } from "./src/db";
 
 const PORT = 3001;
 
-sequelize.sync({ alter: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
